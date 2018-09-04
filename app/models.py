eb17320e3ad9daee619d8ec15fbb2abaa3eb905e
KITHU <james.kithu2@gmail.com>
2018-09-04 10:39:53 +0300

    ft: add models to store data for the app #160244052

---
+++ app/models.py
@@ -0,0 +1,45 @@
+import datetime
+import uuid
+
+class Models(object):
+    database = {
+        "my_questions":[],
+        "my_answers":[]
+    }
+    def __init__(self):
+        
+        self.date = str(datetime.datetime.now().date())
+
+    def get_all_questions(self):
+        all_questions=[]
+        all_questions = self.database["my_questions"]
+        return all_questions
+
+    def add_question(self,question_title,question_body):
+        self.id = uuid.uuid4().hex
+        qst = {"id":self.id,"title":question_title,"Question":question_body,"date":self.date}
+        self.database["my_questions"].append(qst)
+       
+    
+    def get_a_question(self,id):
+        for question in self.database["my_questions"]:
+            if (id in question.values()) == True:
+                ans = self.get_answers_to_a_question(id)
+                question["answers"] = ans
+                return question
+        return "No question with that id"
+
+    def answer_a_question(self,question_id,answer):
+        ans = {"question_id":question_id,"answer":answer}
+        self.database["my_answers"].append(ans)
+
+    def get_answers_to_a_question(self,id):
+        ans = []
+        for answers in self.database["my_answers"]:
+            if (id in answers.values()) == True:
+                ans.append(answers['answer']) 
+        return ans
+
+
+
+        
