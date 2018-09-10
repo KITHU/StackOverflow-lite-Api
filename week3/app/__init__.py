99d6ab78e0282616dacb7b7c3038c5020867440c
KITHU <james.kithu2@gmail.com>
2018-09-10 13:00:57 +0300

    ch: added init file after accidentally deleting it

---
+++ week3/app/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_restplus import Api, namespace
+from flask_restplus import model
+from flask_restplus import fields
+from flask_restplus import Resource
+from flask_restplus import reqparse
+from flask_jwt_extended import JWTManager
+
+
+
+# local import
+from instance.config import app_config
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(app_config[config_name])
+    jwt = JWTManager(app)
+    # app.config.from_pyfile('config.py')
+    
+    # api.add_resource(Questions, '/api/v1/question')
+    from app.apis import apiv1_bp
+    app.register_blueprint(apiv1_bp, url_prefix='/api/v1')
+    # api = Api(app)
+    return app
